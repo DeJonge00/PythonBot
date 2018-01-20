40372673a57de2098f85ba9ae4800bd2b80acf64
dejon <dejon@DESKTOP-1R65MNL>
2018-01-20 15:47:42 +0100

    .name

--- PythonBot/removeMessage.py
+++ PythonBot/removeMessage.py
@@ -6,7 +6,7 @@ async def deleteMessage(bot, ctx, istyping=True):
     try:
         await bot.delete_message(ctx.message)
     except discord.Forbidden:
-        print(ctx.message.server + " | No permission to delete messages")
+        print(ctx.message.server.name + " | No permission to delete messages")
     if istyping:
         await bot.send_typing(ctx.message.channel);
 
@@ -17,6 +17,6 @@ async def nyaCheck(bot, ctx):
         try:
             await bot.delete_message(m)
         except discord.Forbidden:
-            print(ctx.message.server + " | No permission to delete messages")
+            print(ctx.message.server.name + " | No permission to delete messages")
         return False
     return True
