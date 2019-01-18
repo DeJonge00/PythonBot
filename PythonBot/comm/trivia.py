9630545c9303b1f6910382ded8511450368f724b
arnaud <arnaud.sinays@epitech.eu>
2019-01-18 01:32:22 +0100

    trivia game

--- PythonBot/comm/trivia.py
+++ PythonBot/comm/trivia.py
@@ -1,10 +1,9 @@
-from discord.ext import commands
 import discord
 import json
 import html
 import requests
-import re
 import random
+import asyncio
 
 # this game has been made with the open trivia db API : https://opentdb.com/
 # All data provided by the API is available under the Creative Commons Attribution-ShareAlike 4.0 International License.
@@ -12,113 +11,305 @@ import random
 
 class Trivia:
     """Init Trivia game with the number of questions, their category, difficulty and type (boolean or multiple)"""
-    def __init__(self, my_bot):
+    def __init__(self, my_bot, channel, author, categories, mode):
         self.bot = my_bot
-        self.questions_nb = "10"
+        self.questions_nb = ""
+        self.categories = categories
         self.category = ""
         self.difficulty = ""
         self.type = ""
-        print("Trivia started")
+        self.players = []
+        self.mode = mode
+        self.channel = channel
+        self.game_creator = author
+        self.joinable = False
 
-    # {prefix}trivia <categories>
-    @commands.command(pass_context=1, help="Trivia categories", aliases=["tr"])
-    async def trivia(self, ctx, *args):
-        if not await self.bot.pre_command(message=ctx.message, command='trivia'):
+    # setters
+    def set_category(self, new_category):
+        self.category = new_category
+
+    def set_difficulty(self, new_difficulty):
+        self.difficulty = new_difficulty
+
+    def set_type(self, new_type):
+        self.type = new_type
+
+    def set_mode(self, new_mode):
+        self.mode = new_mode
+
+    async def set_questions_nb(self, new_questions_nb):
+        self.questions_nb = int(new_questions_nb)
+
+    async def player_join(self, author):
+        if not self.joinable:
+            await self.bot.say(author.mention + " sorry but it's either too late or there's no game in preparation.")
             return
-        if len(args) <= 0:
-            await self.bot.say(
-                "Start a new game with >trivia new <questions nbr> <category> <difficulty> <type> <mode>\n" +
-                "If nothing is specified, the game will start with 10 questions and the rest will be random.\n" +
-                "You can see categories using >trivia <cat | categories>.\n" +
-                "Difficulty can be set to easy, medium or hard\n" +
-                "Type is the question type (boolean: true/false, multiple: multiple choices)\n" +
-                "Mode can be either time attack (first one getting the right answer wins) or turn by turn\n"
-            )
+        for p in self.players:
+            if author == p.playerid:
+                await self.bot.say(author.mention + " you are already registered for this party, baka!")
+                return
+        self.players.append(TriviaPlayer(author))
+        await self.bot.say(author.mention + " joins the battle!")
+
+    async def player_quit(self, author):
+        for player in self.players:
+            if player.playerid == author:
+                player.quit_game()
+                await self.bot.say(player.playerid.mention + " left the game!")
+                return
+        await self.bot.say(author.mention + " you are currently not in any party on this channel.")
+
+    async def allow_join(self):
+        self.joinable = True
+        await asyncio.sleep(60.0)
+        self.joinable = False
+        await self.bot.say("Game starting!")
+        await self.set_questions_nb(self.questions_nb * len(self.players))
+        await self.start_turns_game()
+
+    async def get_params(self):
+        def is_cat(msg):
+            if msg.content.lower() == "any":
+                return True
+            if is_natural_nbr(msg.content) and \
+                    int(msg.content) in range(self.categories[0]['id'],
+                                              self.categories[len(self.categories) - 1]['id'] + 1):
+                return True
+            return False
+
+        def is_dif(msg):
+            return msg.content.lower() in ["easy", "medium", "hard", "any"]
+
+        def is_type(msg):
+            return msg.content.lower() in ["boolean", "multiple", "any"]
+
+        # CATEGORIES
+        await self.bot.say(
+            "Specify a category number: (use '>trivia categories' to display categories) or type 'any' for random.")
+        category = await self.bot.wait_for_message(channel=self.channel,
+                                                   author=self.game_creator, check=is_cat, timeout=30.0)
+        if category.content.lower() != "any":
+            self.set_category(int(category.content))
+        if await self.is_timeout(category):
             return
 
-        cat_url = "https://opentdb.com/api_category.php"
-        cat_json = requests.get(url=cat_url)
-        categories = json.loads(cat_json.text)['trivia_categories']
-        question_url = "https://opentdb.com/api.php?amount="
-        game_players = {}
-
-        if args[0] == "cat" or args[0] == "categories":
-            display_cat = "Triva categories are: \n"
-            for cat in categories:
-                display_cat += (str(cat['id']) + ") " + cat['name'] + ".\n")
-            await self.bot.say(display_cat)
+        # DIFFICULTY
+        await self.bot.say(
+            "Specify a difficulty: 'easy', 'medium', 'hard' or 'any'.")
+        difficulty = await self.bot.wait_for_message(channel=self.channel,
+                                                     author=self.game_creator, check=is_dif, timeout=30.0)
+        if difficulty.content.lower() != "any":
+            self.set_difficulty(difficulty.content)
+        if await self.is_timeout(difficulty):
             return
-        argsnbr = len(args)
-        j = 2
-        # checking each argument so we don't have to put them in order // or have some left to random
-        if args[0] == "new" and argsnbr >= 2:
-            self.questions_nb = args[1]
-            while j < argsnbr:
-                if args[j].lower() in ["easy", "medium", "hard"]:
-                    self.difficulty = args[j].lower()
-                    j += 1
-                    continue
-                if args[j].lower() in ["boolean", "multiple"]:
-                    self.type = args[j].lower()
-                    j += 1
-                    continue
-                if int(args[j]) in range(categories[0]['id'], categories[len(categories) - 1]['id'] + 1):
-                    self.category = args[j]
-                    j += 1
 
-        param = {'amount': self.questions_nb,
-                 'category': self.category,
-                 'difficulty': self.difficulty,
-                 'type': self.type
-                 }
-        questions_json = requests.get(url=question_url, params=param)
-        questions = json.loads(questions_json.text)['results']
-        # i is the current question number
-        i = 1
-        # looping for each question
+        # QUESTION TYPE
+        await self.bot.say("Specify a question type: 'boolean', 'multiple' or 'any'.")
+        new_type = await self.bot.wait_for_message(channel=self.channel,
+                                                   author=self.game_creator, check=is_type, timeout=30.0)
+        if new_type.content.lower() != "any":
+            self.set_type(new_type.content.lower())
+        if await self.is_timeout(new_type):
+            return
+
+        # TURN MODE
+        if self.mode == "turn":
+            await self.bot.say("How many questions per players?")
+            question_answer = await self.bot.wait_for_message(channel=self.channel,
+                                                              author=self.game_creator, check=is_natural, timeout=30.0)
+            if await self.is_timeout(question_answer):
+                return
+            await self.bot.say("Parameters completed! Each person that now wants to join the game has 60 "
+                               "seconds to use >trivia join on this channel to participate to the upcoming game")
+            await self.set_questions_nb(int(question_answer.content))
+            await self.allow_join()
+
+        # TIME MODE
+        else:
+            await self.bot.say("How many questions? (required)")
+            question_answer = await self.bot.wait_for_message(channel=self.channel,
+                                                              author=self.game_creator, check=is_natural, timeout=30.0)
+            if await self.is_timeout(question_answer):
+                return
+            await self.set_questions_nb(question_answer.content)
+
+    def init_game(self):
+        question_url = "https://opentdb.com/api.php"
+        questions_numb = int(self.questions_nb)
+        questions = []
+        while questions_numb > 0:
+            if questions_numb > 49:
+                request_question = '49'
+            else:
+                request_question = str(questions_numb)
+            param = {'amount': request_question,
+                     'category': self.category,
+                     'difficulty': self.difficulty,
+                     'type': self.type}
+            new_request = json.loads((requests.get(url=question_url, params=param)).text)['results']
+            questions += new_request
+            questions_numb -= 49
+
+        return questions
+
+    async def start_turns_game(self):
+        questions = self.init_game()
+        question_number = 1
+        player_index = 0
+        if len(self.players) < 1:
+            await self.bot.say("No one joined the game, cancelling...")
+            return
         for question in questions:
-            answers_list = question['incorrect_answers']
-            answers_list.append(question['correct_answer'])
-            random.shuffle(answers_list)
-            if question['type'] == "multiple":
-                answers = "1) {}\n2) {}\n3) {}\n4) {}".format(answers_list[0], answers_list[1],
-                                                              answers_list[2], answers_list[3])
-            else:
-                answers = '\n'.join(answers_list)
-            embed = discord.Embed(colour=0x4C4CFF)
-            embed.add_field(name="Question n°", value="{}/{}".format(i, self.questions_nb))
-            embed.add_field(name="Category:", value=html.unescape(question['category']))
-            embed.add_field(name="Question: ", value="**{}**".format(html.unescape(question['question'])))
-            embed.add_field(name="Possible answers: ", value=html.unescape(answers), inline=False)
-            await self.bot.say(embed=embed)
-            i += 1
+            # skip player turn and his question if he left the game
+            if player_index >= len(self.players):
+                player_index = 0
+            if self.players[player_index].isplaying:
+                await self.ask_question(self.players[player_index], question, question_number)
+                question_number += 1
+                player_index += 1
+        await self.bot.say("Game is over! Here's the leaderboard:")
+        await self.display_leaderboard()
 
-            if question['type'] == "boolean":
-                player_answer = await self.bot.wait_for_message(channel=ctx.message.channel)
-                while player_answer.content.lower() != question['correct_answer'].lower():
-                    player_answer = await self.bot.wait_for_message(channel=ctx.message.channel)
+    async def ask_question(self, player, question, question_nb):
+        answers_list = question['incorrect_answers']
+        answers_list.append(question['correct_answer'])
+        random.shuffle(answers_list)
+        if question['type'] == "multiple":
+            answers = "1) {}\n2) {}\n3) {}\n4) {}".format(answers_list[0], answers_list[1],
+                                                          answers_list[2], answers_list[3])
+        else:
+            answers = '\n'.join(answers_list)
+        embed = discord.Embed(colour=0x4C4CFF)
+        embed.add_field(name="Question n°", value="{}/{}".format(question_nb, self.questions_nb))
+        embed.add_field(name="Category:", value=html.unescape(question['category']))
+        embed.add_field(name="Question: ", value="**{}**".format(html.unescape(question['question'])))
+        embed.add_field(name="Possible answers: ", value=html.unescape(answers), inline=False)
+        await self.bot.say(player.playerid.mention + " this question is for you:")
+        await self.bot.say(embed=embed)
+        if question['type'] == "multiple":
+            player_answer = await self.bot.wait_for_message(channel=self.channel,
+                                                            author=player.playerid, check=is_acceptable_answer,
+                                                            timeout=60.0)
+        else:
+            player_answer = await self.bot.wait_for_message(channel=self.channel, author=player.playerid, timeout=60.0)
+        if player_answer is None:
+            await self.bot.say("The question was apparently too complicated for " + player.playerid.mention)
+            return
+        if self.is_answer_correct(question, player_answer.content.lower(), answers_list):
+            player.add_point()
+            await self.bot.say(
+                player.playerid.mention + " correct! You win 1 point. Current score: {}".format(player.score))
+        else:
+            await self.bot.say(player.playerid.mention + " wrong! The correct answer was: {}. Current score: {}".
+                               format(html.unescape(question['correct_answer']), player.score))
 
-            else:
-                k = 0
-                # finds where the correct answer is
-                for k in range(0, 4):
-                    if answers_list[k] == question['correct_answer']:
-                        break
-                player_answer = await self.bot.wait_for_message(channel=ctx.message.channel, content=str(k + 1))
+    @staticmethod
+    def is_answer_correct(question, answer, answers_list):
+        if is_natural_nbr(answer):
+            if answers_list[int(answer) - 1] == question['correct_answer']:
+                return True
+        if answer == question['correct_answer'].lower():
+            return True
+        return False
 
-            # goes out of the loop only if we get a correct answer
-            # scoreboard - uses a dictionary, creates the key if it doesn't exists or adds one point.
-            await self.bot.say("Correct! " + player_answer.author.mention + " scored a point!")
-            if str(player_answer.author) in game_players.keys():
-                game_players[str(player_answer.author)] += 1
-            else:
-                game_players[str(player_answer.author)] = 1
+    async def display_leaderboard(self):
+        embed = discord.Embed(colour=0x4C4CFF)
+        p = 0
+        long_ass_string = ""
+        while p < 10 and p < len(self.players):
+            long_ass_string += "{}: {} points.\n".format(self.players[p].playerid, self.players[p].score)
+            p += 1
+        embed.add_field(name="Trivia Leaderboard", value=long_ass_string)
+        await self.bot.say(embed=embed)
 
-        scoreboard = discord.Embed(colour=0x4C4CFF)
-        score = ""
-        for player in sorted(game_players, key=game_players.get, reverse=True):
-            score += "**{}**".format(player) + " with **{}** point(s)!\n".format(game_players[player])
-        scoreboard.add_field(name="Scoreboard", value=score, inline=False)
+    async def is_timeout(self, msg):
+        if msg is None:
+            await self.bot.say("game creation timed out :sob:")
+            return True
+        return False
 
-        await self.bot.say("No more questions left! Here's the scoreboard for this game:")
-        await self.bot.say(embed=scoreboard)
+
+def is_natural(msg):
+    try:
+        nbr = int(msg.content)
+        return 0 < nbr < 6000
+    except Exception:
+        return False
+
+
+def is_acceptable_answer(msg):
+    try:
+        nbr = int(msg.content)
+        return 1 <= nbr <= 4
+    except Exception:
+        return False
+
+
+def is_natural_nbr(msg):
+    if msg.isdigit() and int(msg) > 0:
+        return True
+    return False
+
+
+class TriviaPlayer:
+    def __init__(self, playerid):
+        self.playerid = playerid
+        self.isplaying = True
+        self.score = 0
+
+    def add_point(self):
+        self.score += 1
+
+    def quit_game(self):
+        self.isplaying = False
+
+
+
+
+    # # {prefix}trivia <categories>
+    # @commands.command(pass_context=1, help="Trivia", aliases=["tr"])
+    # async def trivia(self, ctx, *args):
+    #     if not await self.bot.pre_command(message=ctx.message, command="trivia"):
+    #         return
+    #     if len(args) <= 0:
+    #         await self.bot.say("[Usage] To start a new game use: >trivia new <mode>('turns' or 'time')")
+    #         return
+    #     categories_url = "https://opentdb.com/api_category.php"
+    #
+    #
+    #     if args[0] == "cat" or args[0] == "categories":
+    #         await self.display_categories()
+    #         return
+    #
+    #     if args[0] == "join":
+    #         if not self.joinable:
+    #             await self.bot.say(
+    #                 self.game_creator.mention + " sorry but it's either too late or there's no game in preparation.")
+    #             return
+    #         for p in self.players:
+    #             if self.game_creator == p.playerid:
+    #                 await self.bot.say(self.game_creator.mention + " you are already registered for this party, baka!")
+    #                 return
+    #         self.player_join(self.game_creator)
+    #         await self.bot.say(self.game_creator.mention + " joins the battle!")
+    #
+    #     if args[0] == "quit":
+    #         for p in self.players:
+    #             if p.playerid == self.game_creator:
+    #                 self.players.remove(p)
+    #                 return
+    #         await self.bot.say(self.game_creator.mention + " you are not registered in a party!")
+    #
+    #     if args[0] == "new":
+    #         await self.bot.say("New trivia game requested!\nPlease chose a game mode: 1)time attack  2)turn by turn")
+    #         game_mode = await self.bot.wait_for_message(channel=self.channel, author=self.game_creator)
+    #         if game_mode.content == '1':
+    #             await self.bot.say("Time attack mode selected!")
+    #             self.set_mode("time")
+    #         elif game_mode.content == '2':
+    #             await self.bot.say("Turn by turn mode selected!")
+    #             self.set_mode("turn")
+    #         else:
+    #             await self.bot.say(self.game_creator.mention + " stop wasting my time.")
+    #             return
+    #         await self.get_params(ctx)
